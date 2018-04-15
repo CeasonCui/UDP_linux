2ac12fb43e77e2663d1c2249c00a191f30cf6011
CeasonCui <ceasoncui@gmail.com>
2018-04-15 20:58:52 +0800

    init

---
+++ client/udpclifileup.c
@@ -0,0 +1,135 @@
+#include        <netinet/in.h>
+#include        <errno.h>
+#include        <stdio.h>
+#include        <stdlib.h>
+#include        <sys/socket.h>
+#include        <unistd.h>
+#include        <string.h>
+#define MAXLINE                  4096
+#define LISTENQ                  1024    /* 2nd argument to listen() */
+#define SERV_PORT                9877
+#define SA      struct sockaddr
+
+
+void dg_cli(FILE *, int, const SA *, socklen_t);
+
+main(int argc, char **argv)
+{
+	int sockfd;
+	struct sockaddr_in servaddr;
+
+	if (argc != 2) {
+		printf("usage:udpcli01sigio <IPaddress>\n");
+		exit(1);
+	}
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0))<0) {
+		printf("socket error.");
+		exit(1);
+	}
+
+	dg_cli(stdin, sockfd, (SA *)&servaddr, sizeof(servaddr));
+	
+	exit(0);
+}
+
+void
+dg_cli(FILE *fp, int sockfd, const SA *pservaddr, socklen_t servlen)
+{
+	int  n;
+	char sendline[MAXLINE], recvline[MAXLINE + 1], recvline2[MAXLINE + 1],recvline3[MAXLINE + 1];
+	const char *s="1\n";
+	const char *s2="2\n";
+	const char *s3="resent.";
+	char buffer[MAXLINE+1];
+	char filebuffer[8000];
+	char file_name[256]="clireceived.txt";
+	printf("========================GUIDE========================\n");
+	printf("| 1:get service menu                                |\n");
+	printf("| 2xxx.x:download a file from service  eg.2text.txt |\n");
+	printf("| 3xxx.x:up a file from client  eg.3text.txt        |\n");
+	printf("| enter any except 1,2,3                            |\n");
+	printf("=====================================================\n");
+	printf("\n");
+	while (fgets(sendline, MAXLINE,fp) != NULL) {
+		//sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+		//printf("sent");
+		if(*sendline=='3'){
+			char file_name_up[256];
+			bzero(file_name_up,256);
+			for(int i=1;sendline[i]!='\n';i++){
+				file_name_up[i-1]=sendline[i];
+			}
+			printf("filename:%s\n",file_name_up);
+			FILE *fp1=fopen(file_name_up,"r");
+			if(fp1==NULL){
+				printf("FILE:%s Not Found\n",file_name_up);
+				continue;
+			}
+			else{
+				sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+				bzero(filebuffer,0);
+				int length=0;
+				while((length = fread(filebuffer,sizeof(char),8000,fp1))>0){
+						//printf("length=%d\n",length);
+						
+					if(sendto(sockfd,filebuffer,length, 0, pservaddr, servlen)<0){
+						printf("Send File:%s Failed.\n",file_name_up);
+						break;
+					}
+					fputs(filebuffer, stdout);
+					bzero(filebuffer,8000);
+				}
+				fclose(fp1);
+				printf("File:%s Transfer Successful!\n",file_name_up);
+			}		
+		}
+		else{
+			sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+		}
+		while(n = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL)){
+			//printf("%d\n",n);
+			//printf("%s\n",recvline);
+			if(strcmp(recvline,s3)==0){
+				FILE *fp=fopen(file_name,"w");
+				if(fp==NULL){
+					printf("file can not open\n");
+					exit(1);
+				}
+				bzero(buffer,MAXLINE+1);
+				int length=0;
+				while(length = recvfrom(sockfd, buffer, MAXLINE, 0, NULL, NULL)){
+					printf("length=%d\n",length);
+					if(length<=20){
+						break;
+					}
+					if(fwrite(buffer,sizeof(char),length,fp)<length){
+						printf("file write faile\n");
+						break;
+					}
+					fputs(buffer, stdout);
+					bzero(buffer,MAXLINE+1);
+					fputs(buffer,stdout);
+					recvline[n] = 0; 
+					
+				}
+				printf("receive file successful\n");
+				fclose(fp);
+			}
+			recvline[n] = 0; /* null terminate */
+			fputs(recvline, stdout);
+			printf("\n");
+			if(n <100)
+				break;
+			
+		}
+		
+	}
+}
+ 
---
+++ server/udpservfileup.c
@@ -0,0 +1,180 @@
+#include        <netinet/in.h>
+#include        <errno.h>
+#include        <stdio.h>
+#include        <stdlib.h>
+#include        <sys/socket.h>
+#include        <unistd.h>
+#include	<dirent.h>
+#define MAXLINE                  4096
+#define LISTENQ                  1024    /* 2nd argument to listen() */
+#define SERV_PORT                9877
+#define SA      struct sockaddr
+
+static int sockfd;
+void dg_echo(int, SA *, socklen_t);
+
+int 
+main(int argc, char ** argv)
+{
+	struct sockaddr_in servaddr, cliaddr;	
+	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0))<0) {
+		printf("socket error.\n");
+		exit(1);
+	}
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
+
+	bind(sockfd, (SA *)&servaddr, sizeof(servaddr));
+
+	dg_echo(sockfd, (SA *)&cliaddr, sizeof(cliaddr));
+}
+
+int readFileList(char *basePath,SA *pcliaddr,socklen_t clilen){
+	DIR *dir;
+	struct dirent *ptr;
+	char base[1000];
+	char path[1000];
+	socklen_t len;
+	len = clilen;
+	if((dir = opendir(basePath))==NULL){
+		perror("Open dir error...");
+		exit(1);
+	}
+	
+	while((ptr = readdir(dir)) != NULL){
+		if(strcmp(ptr->d_name,".")==0||strcmp(ptr->d_name,"..")==0)
+			continue;
+		else if(ptr->d_type == 8){
+			memset(path,'\0',sizeof(path));
+			strcpy(path,basePath);
+			strcat(path,"/");
+			strcat(path,ptr->d_name);
+			//path=basePath;
+			sendto(sockfd,path, sizeof(path), 0, pcliaddr, len);
+			//printf("d_name:%s/%s\n",basePath,ptr->d_name);
+			}
+		else if(ptr->d_type == 10){
+			memset(path,'\0',sizeof(path));
+			strcpy(path,basePath);
+			strcat(path,"/");
+			strcat(path,ptr->d_name);
+			sendto(sockfd,path, sizeof(path), 0, pcliaddr, len);
+			//printf("d_name:%s/%s\n",basePath,ptr->d_name);
+			}
+		else if(ptr->d_type == 4){
+			memset(base,'\0',sizeof(base));
+			strcpy(base,basePath);
+			strcat(base,"/");
+			strcat(base,ptr->d_name);
+			readFileList(base,pcliaddr,clilen);
+		}
+	}
+	closedir(dir);
+	return 1;
+}
+
+void 
+dg_echo(int sockfd, SA *pcliaddr, socklen_t clilen)
+{
+	DIR *dir;
+	char basePath[1000];
+	const char *s1="1";
+	const char *s2="2\n";
+	char s3[1000]="resent.";
+	char file_name_up[256]="servreceived.txt";
+	//const char *s4="udpserv1.c";
+	int n;
+	socklen_t len;
+	char mesg[MAXLINE];
+	char mesg_n[MAXLINE];
+	char buffer[80];
+	char filebuffer[8000];
+	char fileupbuffer[8000];
+	char filetap[1000]="Please enter the file name: ";
+	getcwd(buffer,sizeof(buffer));
+	//printf("dir is:%s\n",buffer);
+	if(buffer == NULL){
+		perror("getcwd error");
+	}
+	else{
+		//printf("%s\n",buffer);
+		//free(buffer);
+	}
+	printf("dir is:%s\n",buffer);
+	
+		
+	for ( ; ; ) {
+		len = clilen;
+		memset(mesg_n,'\0',sizeof(mesg_n));
+		n = recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
+		for(int i=0;mesg[i]!='\n';i++){
+			mesg_n[i]=mesg[i];
+		}
+		printf("mesg_n:%s\n",mesg_n);
+		if(strcmp(mesg_n,s1)==0){
+			printf("in");
+			readFileList(buffer,pcliaddr,clilen);
+		}
+		else if(*mesg=='2'){
+			char file_name[256];
+			bzero(file_name,256);
+			sendto(sockfd, s3, n, 0, pcliaddr, len);
+			
+			for(int i=1;mesg[i]!='\n';i++){
+				file_name[i-1]=mesg[i];
+			}
+			printf("change to %s\n",file_name);
+			FILE*fp = fopen(file_name,"r");
+			if(fp==NULL){
+				printf("FILE:%s Not Found\n",file_name);
+			}
+			else{
+				bzero(filebuffer,0);
+				int length=0;
+				while((length = fread(filebuffer,sizeof(char),8000,fp))>0){
+					printf("length=%d\n",length);
+					if(sendto(sockfd,filebuffer,length, 0, pcliaddr, len)<0){
+						printf("Send File:%s Failed.\n",file_name);
+						break;
+					}
+					bzero(filebuffer,8000);
+				}
+				fclose(fp);
+				printf("File:%s Transfer Successful!\n",file_name);
+			}			
+		}
+		else if(*mesg=='3'){
+			//printf("intter 3\n");
+			FILE *fp1=fopen(file_name_up,"w");
+			if(fp1==NULL){
+				printf("file can not open\n");
+				exit(1);
+			}
+			bzero(fileupbuffer,MAXLINE+1);
+			int lengthup=0;
+			lengthup = recvfrom(sockfd, fileupbuffer, MAXLINE, 0, NULL, NULL);
+				/*if(lengthup<=10){
+					break;
+				}*/
+				//printf("intter\n");
+				printf("%d\n",lengthup);
+				if(fwrite(fileupbuffer,sizeof(char),lengthup,fp1)<lengthup){
+					printf("file write faile\n");
+					break;
+				}
+				fputs(fileupbuffer, stdout);
+				bzero(fileupbuffer,MAXLINE+1);
+				//fputs(fileupbuffer,stdout);
+				//mesg[lengthup] = 0; 
+					
+			//}
+			printf("receive file successful\n");
+			fclose(fp1);
+		}
+		printf("recv\n");
+		sendto(sockfd, mesg, n, 0, pcliaddr, len);
+		printf("recv2\n");
+	}
+}
